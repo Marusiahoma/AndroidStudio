0d55ee8cff6d25e999b8d6ebe1c74c95790d3b5e
Marusiahoma <57797383+Marusiahoma@users.noreply.github.com>
2024-05-24 19:42:29 +0300

    first commit

---
+++ yandexdriver/src/main/java/ru/mirea/pinashiname/yandexdriver/App.kt
@@ -0,0 +1,17 @@
+package ru.mirea.pinashiname.yandexdriver
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory
+
+class App : Application() {
+
+    companion object {
+        private const val MAPKIT_API_KEY = "a44e6c7f-0ef3-4160-836a-4af1ee304664"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+    }
+}
